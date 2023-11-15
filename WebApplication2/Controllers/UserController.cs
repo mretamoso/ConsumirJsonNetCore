59d791a486dcc7e3426693c46b970c46298fdbd7
MiguelAlonso <miguelretamosoin@gmail.com>
2023-11-14 19:04:53 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿namespace WebApplication2.Controllers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+public class UserController:Controller
+    {
+
+    [HttpGet("api/User/GetAll")]
+    [Produces("application/json")]
+    public async Task<IActionResult> GetAll()
+    {
+       
+        string apiUrl = "https://jsonplaceholder.typicode.com/users";
+
+        using (HttpClient httpClient = new HttpClient())
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonString = await response.Content.ReadAsStringAsync();
+                List<UsuariosModel> userList = JsonConvert.DeserializeObject<List<UsuariosModel>>(jsonString);
+
+                return new JsonResult(userList);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, "Error al obtener los usuarios");
+            }
+        }
+    }
+
+}
+
---
+++ WebApplication2/DbContext.cs
@@ -0,0 +1,14 @@
+﻿//using System.Collections.Generic;
+
+//namespace WebApplication2
+//{
+//    public class ApplicationDbContext : ApplicationDbContext
+//    {
+//        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        public DbSet<Usuario> Usuarios { get; set; }
+//    }
+//}
